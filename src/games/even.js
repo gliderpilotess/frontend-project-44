82b87b05834b30b11d1f8871294ad716b31e0c4d
Eva Ya <pakalkaeva@gmail.com>
2024-02-07 01:22:24 +0500

    add change even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,14 +1,8 @@
 // games brain-even
-
 const isEven = (num) => num % 2 === 0;
 
-let hasPrintedMessage = false;
-
 const generateRound = () => {
-  if (!hasPrintedMessage) {
-    console.log("Answer 'yes' if the number is even, otherwise answer 'no'");
-    hasPrintedMessage = true;
-  }
+  console.log("Answer 'yes' if the number is even, otherwise answer 'no'");
   const randomNumber = Math.floor(Math.random() * 100) + 1;
   const question = `${randomNumber}`;
   const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
