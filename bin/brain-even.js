b0353b49cc3ff62c2bcc57f6d25a0d7fcb27735e
Eva Ya <pakalkaeva@gmail.com>
2024-02-07 19:52:06 +0500

    add change even.js #9

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,40 @@
 #!/usr/bin/env node
+import readlineSync from 'readline-sync';
 
-import runGame from '../src/constants/index.js';
-import game from '../src/games/even.js';
+function welcomeUser() {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  return name;
+}
 
-runGame(game);
+function isEven(number) {
+  return number % 2 === 0;
+}
+
+function playGame(userName) {
+  let correctAnswersCount = 0;
+
+  while (correctAnswersCount < 3) {
+    const number = Math.floor(Math.random() * 100) + 1;
+    console.log("Answer 'yes' if the number is even, otherwise answer 'no'.");
+    console.log(`Question: ${number}`);
+    const userAnswer = readlineSync.question('Your answer: ').toLowerCase();
+
+    if ((userAnswer === 'yes' && isEven(number)) || (userAnswer === 'no' && !isEven(number))) {
+      console.log('Correct!');
+      correctAnswersCount += 1;
+    } else {
+      const correctAnswer = isEven(number) ? 'yes' : 'no';
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return false;
+    }
+  }
+
+  console.log(`Congratulations, ${userName}!`);
+  return true;
+}
+
+const userName = welcomeUser();
+playGame(userName);
