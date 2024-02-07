67fb123cb32302bd162d8249f9912139264544c7
Eva Ya <pakalkaeva@gmail.com>
2024-02-07 20:24:19 +0500

    add change even.js #11

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -3,9 +3,9 @@ import readlineSync from 'readline-sync';
 
 function welcomeUser() {
   console.log('Welcome to the Brain Games!');
-  console.log("Answer 'yes' if the number is even, otherwise answer 'no'.");
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
+  console.log("Answer 'yes' if the number is even, otherwise answer 'no'.");
   return name;
 }
 
