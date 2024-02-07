243442bbe4945a463049443553bcf60485273fb9
Eva Ya <pakalkaeva@gmail.com>
2024-02-07 23:41:02 +0500

    add change games

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,6 +1,66 @@
 #!/usr/bin/env node
+import readlineSync from 'readline-sync';
 
-import runGame from '../src/constants/index.js';
-import calcGame from '../src/games/calc.js';
+const getRandomNumber = () => Math.floor(Math.random() * 100);
 
-runGame(calcGame);
+const getRandomOperator = () => {
+  console.log('What is the result of the expression?');
+  const operators = ['+', '-', '*'];
+  const randomIndex = Math.floor(Math.random() * operators.length);
+  return operators[randomIndex];
+};
+
+const calculateExpression = (num1, operator, num2) => {
+  switch (operator) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    default:
+      return NaN;
+  }
+};
+
+const generateRound = () => {
+  const num1 = getRandomNumber();
+  const num2 = getRandomNumber();
+  const operator = getRandomOperator();
+  const question = `${num1} ${operator} ${num2}`;
+  const correctAnswer = String(calculateExpression(num1, operator, num2));
+
+  return { question, correctAnswer };
+};
+
+const welcomeUser = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  return name;
+};
+
+const playGame = (userName) => {
+  let correctAnswersCount = 0;
+
+  while (correctAnswersCount < 3) {
+    const { question, correctAnswer } = generateRound();
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+      correctAnswersCount += 1;
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return false;
+    }
+  }
+
+  console.log('Congratulations!');
+  return true;
+};
+
+const userName = welcomeUser();
+playGame(userName);
--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,6 +1,62 @@
 #!/usr/bin/env node
+import readlineSync from 'readline-sync';
 
-import runGame from '../src/constants/index.js';
-import progressionGame from '../src/games/prog.js';
+const getRandomArithmeticProgression = (length) => {
+  const start = Math.floor(Math.random() * 10);
+  const diff = Math.floor(Math.random() * 5) + 1;
+  const progression = [];
 
-runGame(progressionGame);
+  for (let i = 0; i < length; i += 1) {
+    progression.push(start + i * diff);
+  }
+
+  return progression;
+};
+
+let hasPrintedMessage = false;
+
+const generateRound = () => {
+  if (!hasPrintedMessage) {
+    console.log('What number is missing in the progression?');
+    hasPrintedMessage = true;
+  }
+  const progressionLength = Math.floor(Math.random() * 6) + 5;
+  const progression = getRandomArithmeticProgression(progressionLength);
+  const hiddenIndex = Math.floor(Math.random() * progression.length);
+  const correctAnswer = progression[hiddenIndex].toString();
+  progression[hiddenIndex] = '..';
+  const question = progression.join(' ');
+  return { question, correctAnswer };
+};
+
+const welcomeUser = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  return name;
+};
+
+const playGame = (userName) => {
+  let correctAnswersCount = 0;
+
+  while (correctAnswersCount < 3) {
+    const { question, correctAnswer } = generateRound();
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+      correctAnswersCount += 1;
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return false;
+    }
+  }
+
+  console.log('Congratulations!');
+  return true;
+};
+
+const userName = welcomeUser();
+playGame(userName);
