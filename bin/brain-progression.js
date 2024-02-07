5a67121748a040c302638dfb681718a56e320cc1
Eva Ya <pakalkaeva@gmail.com>
2024-02-08 01:12:38 +0500

    prog

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -33,6 +33,7 @@ const welcomeUser = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
+  console.log('What number is missing in the progression?');
   return name;
 };
 
